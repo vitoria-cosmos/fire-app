1c1ed4b7bd68db9e63418cdefa94cbf1772e345d
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-10-02 14:44:28 -0300

    temp

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './app.css';
 
 import { useState } from 'react';
 
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore';
 // getDoc é pra gente buscar um item
+// getDocs para buscar mais de um item
 
 function App() {
 
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+
+  const [posts, setPosts] = useState([]);
 
 
   async function handleAdd() {
@@ -55,19 +58,19 @@ function App() {
   async function buscarPost() {
     // alert('TESTE')
 
-    const postRef = doc(db, "posts", "12345")
+    // const postRef = doc(db, "posts", "12345")
 
-    await getDoc(postRef)
-    .then((snapshot) => {
-      // O snapshot é para acessar os dados
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   // O snapshot é para acessar os dados
 
-      setAutor(snapshot.data().autor)
-      setTitulo(snapshot.data().titulo)
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
 
-    })
-    .catch(() => {
-      console.log("ERRO AO BUSCAR")
-    })
+    // })
+    // .catch(() => {
+    //   console.log("ERRO AO BUSCAR")
+    // })
 
     // primeiro parametro é a conexão do nosso banco, depois a collection que 
     // queremos acessar, depois, o id do doc queremos saber seus valores
@@ -75,6 +78,31 @@ function App() {
 
     // quando clicarmos no botaço de buscar, ele tem que setar nos campos os valores 
     // que estão ligados ao id especificado na função
+
+
+    // agora vamos buscar vários posts
+    const postsRef = collection(db, "posts")
+
+    // acessa a nossa query
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+      // vamos percorrer cada documento e colocar na lista cada objeto com suas informações
+
+      setPosts(lista);
+    })
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR: ", error)
+    })
+
   }
 
   
@@ -108,6 +136,17 @@ function App() {
 
         <button onClick={handleAdd}>Cadastrar</button>
         <button onClick={buscarPost}>Buscar post</button>
+
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <span>Título: {post.titulo}</span>
+                <span>Autor: {post.autor}</span>
+              </li>
+            )
+          })}
+        </ul>
 
       </div>
       
